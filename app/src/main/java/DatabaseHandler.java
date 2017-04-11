b3dfccaed525296c4fce674b1a74ae70559ae7d0
ilya yudkovich <iyudkovich@paytronix.com>
2017-04-11 14:42:31 -0400

    Added create operation for user

---
+++ app/src/main/java/DatabaseHandler.java
@@ -0,0 +1,84 @@
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+
+import samletcher.huckordump.User;
+
+/**
+ * Created by iyudkovich on 4/11/17.
+ */
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+    // All Static variables
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "HuckOrDump";
+
+    // Users table name
+    private static final String TABLE_Users = "Users";
+
+    // Users Table Columns names
+    private static final String KEY_ID = "ID";
+    private static final String KEY_FIRST_NAME = "First Name";
+    private static final String KEY_LAST_NAME = "Last name";
+    private static final String KEY_GENDER = "Gender";
+    private static final String KEY_Interested_In = "InterestedIn";
+    private static final String KEY_TEAM = "Team";
+    private static final String KEY_POSITION = "Position";
+    private static final String KEY_PICTURE = "Picture";
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // Creating Tables
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_Users + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_FIRST_NAME + " TEXT,"
+                + KEY_LAST_NAME + " TEXT" +
+                KEY_GENDER + " INTEGER" +
+                KEY_Interested_In + " INTEGER" +
+                KEY_TEAM + " INTEGER" +
+                KEY_POSITION + " TEXT" +
+                KEY_PICTURE + " BLOB" +
+                ")";
+        db.execSQL(CREATE_USER_TABLE);
+    }
+
+    // Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_Users);
+
+        // Create tables again
+        onCreate(db);
+    }
+
+    // Adding a new User
+    public void addUser(User user) {
+        // get the database
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // the value that we will eventually add
+        ContentValues values = new ContentValues();
+
+        values.put(KEY_FIRST_NAME, user.getFirst_name());
+        values.put(KEY_LAST_NAME, user.getLast_name());
+        values.put(KEY_GENDER, user.getGender());
+        values.put(KEY_Interested_In, user.getInterestedIn());
+        values.put(KEY_TEAM, user.getTeam_id());
+        values.put(KEY_Interested_In, user.getInterestedIn());
+        values.put(KEY_POSITION, user.getPosition());
+        values.put(KEY_PICTURE, user.getPicture());
+
+        // insert the values for new user
+        db.insert(TABLE_Users, null, values);
+        db.close();
+    }
+
+}
--- app/src/main/java/samletcher/huckordump/User.java
+++ app/src/main/java/samletcher/huckordump/User.java
@@ -5,15 +5,15 @@ package samletcher.huckordump;
  */
 
 public class User {
-    int id;
-    String first_name;
-    String last_name;
+    private int id;
+    private String first_name;
+    private String last_name;
     // 0 is male, 1 is female
-    boolean gender;
-    boolean interest;
-    int team_id;
-    String position;
-    String picture;
+    private boolean gender;
+    private boolean interest;
+    private int team_id;
+    private String position;
+    private String picture;
 
     public User(int id, String first_name, String last_name, boolean gender, boolean interest, int team_id, String position, String picture) {
         this.id = id;
@@ -23,6 +23,86 @@ public class User {
         this.interest = interest;
         this.team_id = team_id;
         this.position = position;
+        this.picture = picture;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public int getGender() {
+        if (isGender()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public boolean isInterest() {
+        return interest;
+    }
+
+    public int getInterestedIn() {
+        if (isInterest()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public void setInterest(boolean interest) {
+        this.interest = interest;
+    }
+
+    public int getTeam_id() {
+        return team_id;
+    }
+
+    public void setTeam_id(int team_id) {
+        this.team_id = team_id;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
         this.picture = picture;
     }
 }
